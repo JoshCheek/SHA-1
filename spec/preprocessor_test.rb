5053e1a529b5a0efec8826d00a45820ec8a11a71
Josh Cheek <josh.cheek@gmail.com>
2016-01-06 11:46:06 -0700

    Consolidate tests that are all telling us the same thing

--- spec/preprocessor_test.rb
+++ spec/preprocessor_test.rb
@@ -16,33 +16,20 @@ class PreprocessorTest < Minitest::Test
     assert_equal '', word_to_binary('')
   end
 
-  def test_converts_lower_case_letter_to_binary
+  def test_word_to_binary_converts_strings_to_binary
     expected = '01100001'
-
     assert_equal expected, word_to_binary('a')
-  end
 
-  def test_converts_different_lower_case_letter_to_binary
     expected = '01111010'
-
     assert_equal expected, word_to_binary('z')
-  end
 
-  def test_converts_upper_case_letter_to_binary
     expected = '01000100'
-
     assert_equal expected, word_to_binary('D')
-  end
 
-  def test_converts_different_upper_case_letter_to_binary
     expected = '01010110'
-
     assert_equal expected, word_to_binary('V')
-  end
 
-  def test_converts_entire_string_to_binary
     expected = '011000010110001001100011'
-
     assert_equal expected, word_to_binary('abc')
   end
 
